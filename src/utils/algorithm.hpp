be2708f24cd4c6bc39a71a63da93d91401adcdf6
jolatechno <joseph.touzet@gmail.com>
2021-11-18 21:22:51 +0100

    trying to improve partitions

--- src/utils/algorithm.hpp
+++ src/utils/algorithm.hpp
@@ -18,9 +18,9 @@ function to partition into n section according to the modulo of an array element
 
 !!!! n_segment MUST be a power of two !!!!
 */
-void generalized_modulo_partition_power_of_two(size_t *idx_begin, size_t *idx_end, size_t const *begin, int *offset, int n_segment, int shift = 0) {
+/*void generalized_modulo_partition_power_of_two(size_t *idx_begin, size_t *idx_end, size_t const *begin, int *offset, int n_segment, int shift = 0) {
 	/* limit values */
-	size_t n_element = std::distance(idx_begin, idx_end);
+	/*size_t n_element = std::distance(idx_begin, idx_end);
 	offset[0] = 0;
 	offset[n_segment] = n_element;
 
@@ -91,11 +91,11 @@ void generalized_modulo_partition_power_of_two(size_t *idx_begin, size_t *idx_en
 			} else
 				std::swap(idx_begin[--end[key]], idx_begin[i]);
 		}
-}
+}*/
 
 void generalized_modulo_partition_power_of_two(size_t *idx_begin, size_t *idx_end, size_t const *begin, int *offset, int n_segment) {
 	/* limit values */
-	/*size_t n_element = std::distance(idx_begin, idx_end);
+	size_t n_element = std::distance(idx_begin, idx_end);
 	offset[0] = 0;
 
 	if (n_segment <= 1) {
@@ -106,7 +106,7 @@ void generalized_modulo_partition_power_of_two(size_t *idx_begin, size_t *idx_en
 	const size_t bitmask = n_segment - 1;
 
 	/* count occurences */
-	/*int *end = new int[n_segment]();
+	int *end = new int[n_segment]();
 	#pragma omp parallel
 	{
 		int *count = new int[n_segment]();
@@ -119,18 +119,18 @@ void generalized_modulo_partition_power_of_two(size_t *idx_begin, size_t *idx_en
 		}
 
 		/* global reduction */
-		/*for (int i = 0; i < n_segment; ++i)
+		for (int i = 0; i < n_segment; ++i)
 			#pragma omp atomic
 			end[i] += count[i];
 	}
 
 	/* compute the end of each segment */
-	/*__gnu_parallel::partial_sum(end, end + n_segment, offset + 1);
+	__gnu_parallel::partial_sum(end, end + n_segment, offset + 1);
 	for (int i = 0; i < n_segment; ++i)
 		end[i] = offset[i + 1];
 
 	/* move elements */
-	/*int partition = 0;
+	int partition = 0;
 	for (size_t i = 0; i < n_element;)
 		if (i >= end[partition]) {
 			i = offset[++partition];
@@ -141,4 +141,4 @@ void generalized_modulo_partition_power_of_two(size_t *idx_begin, size_t *idx_en
 			} else
 				std::swap(idx_begin[--end[key]], idx_begin[i]);
 		}
-}*/
+}
