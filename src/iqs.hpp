bb5947567ac074db7fe2d26ff2cad88c435a1ecf
jolatechno <joseph.touzet@gmail.com>
2022-02-12 14:34:50 +0100

    trying to improve omp collisions

--- src/iqs.hpp
+++ src/iqs.hpp
@@ -509,8 +509,45 @@ namespace iqs {
 					utils::load_balancing_from_prefix_sum(total_partition_begin.begin(), total_partition_begin.end(),
 						load_balancing_begin.begin(), load_balancing_begin.end());
 
-				/* rework indexes */
-				for (int i = 1; i <= num_threads; ++i)
+				if (thread_id == 0)
+					mid_step_function("compute_collisions - insert");
+
+				/* insert into separate hashmaps */
+				for (int j = load_balancing_begin[thread_id]; j < load_balancing_begin[thread_id + 1]; ++j) {
+					size_t total_size = 0;
+					for (int other_thread_id = 0; other_thread_id < num_threads; ++other_thread_id) {
+						size_t begin = partition_begin[(num_bucket + 1)*other_thread_id + j];
+						size_t end = partition_begin[(num_bucket + 1)*other_thread_id + j + 1];
+
+						total_size += end - begin;
+					}
+
+					elimination_map.reserve(total_size);
+
+					for (int other_thread_id = 0; other_thread_id < num_threads; ++other_thread_id) {
+						size_t other_oid_begin = load_begin[other_thread_id];
+
+						size_t begin = partition_begin[(num_bucket + 1)*other_thread_id + j] + other_oid_begin;
+						size_t end = partition_begin[(num_bucket + 1)*other_thread_id + j + 1] + other_oid_begin;
+
+						for (size_t i = begin; i < end; ++i) {
+							size_t oid = next_oid[i];
+
+							/* accessing key */
+							auto [it, unique] = elimination_map.insert({hash[oid], oid});
+							if (!unique)
+								/* if it exist add the probabilities */
+								magnitude[it->second] += magnitude[oid];
+							/* keep the object if it is unique */
+							is_unique[oid] = unique;
+						}
+					}
+
+					elimination_map.clear();
+				}
+
+
+				/*for (int i = 1; i <= num_threads; ++i)
 					modulo_offset[(num_threads + 1)*thread_id + i] = partition_begin[(num_bucket + 1)*thread_id + load_balancing_begin[i]];
 
 				#pragma omp barrier
@@ -536,20 +573,20 @@ namespace iqs {
 					for (size_t i = begin; i < end; ++i) {
 						size_t oid = next_oid[i];
 
-						/* accessing key */
+						/* accessing key *//*
 						auto [it, unique] = elimination_map.insert({hash[oid], oid});
 						if (!unique)
-							/* if it exist add the probabilities */
+							/* if it exist add the probabilities *//*
 							magnitude[it->second] += magnitude[oid];
-						/* keep the object if it is unique */
+						/* keep the object if it is unique *//*
 						is_unique[oid] = unique;
 					}
-				}
+				}*/
 
 				if (thread_id == 0)
 					mid_step_function("compute_collisions - finalize");
 
-				elimination_map.clear();
+				//elimination_map.clear();
 			}
 
 			/* check for zero probability */
