cddd396eafdb18fe1027d25c2a902d7b914138d7
jolatechno <joseph.touzet@gmail.com>
2022-05-18 20:51:45 +0200

    updated the doxygen

--- docs/search/all_e.js
+++ docs/search/all_e.js
@@ -2,7 +2,7 @@ var searchData=
 [
   ['random_5fgenerator_0',['random_generator',['../classquids_1_1utils_1_1random__generator.html',1,'quids::utils']]],
   ['receive_5fobjects_1',['receive_objects',['../classquids_1_1mpi_1_1mpi__iteration.html#a30e7a9e5c0175fd1f3f274e104f12450',1,'quids::mpi::mpi_iteration']]],
-  ['resize_2',['resize',['../classquids_1_1utils_1_1fast__vector.html#a6c981139e6aa925b275718ce194c014f',1,'quids::utils::fast_vector']]],
+  ['resize_2',['resize',['../classquids_1_1utils_1_1fast__vector.html#af0d9ea686f8deb18acea5cf22ae3183b',1,'quids::utils::fast_vector']]],
   ['rule_3',['rule',['../classquids_1_1rule.html',1,'quids::rule'],['../classquids_1_1rule.html#ab5963493c6aa55d4893460fd92a97c1c',1,'quids::rule::rule()']]],
   ['rule_5ft_4',['rule_t',['../namespacequids.html#a62455c8f5240a575676aace3a25dfae3',1,'quids']]]
 ];
--- docs/search/functions_a.js
+++ docs/search/functions_a.js
@@ -1,6 +1,6 @@
 var searchData=
 [
   ['receive_5fobjects_0',['receive_objects',['../classquids_1_1mpi_1_1mpi__iteration.html#a30e7a9e5c0175fd1f3f274e104f12450',1,'quids::mpi::mpi_iteration']]],
-  ['resize_1',['resize',['../classquids_1_1utils_1_1fast__vector.html#a6c981139e6aa925b275718ce194c014f',1,'quids::utils::fast_vector']]],
+  ['resize_1',['resize',['../classquids_1_1utils_1_1fast__vector.html#af0d9ea686f8deb18acea5cf22ae3183b',1,'quids::utils::fast_vector']]],
   ['rule_2',['rule',['../classquids_1_1rule.html#ab5963493c6aa55d4893460fd92a97c1c',1,'quids::rule']]]
 ];
