0c0b3a0d46a70842fc76f0be9d3d3d9bb18350b2
jolatechno <joseph.touzet@gmail.com>
2022-05-18 11:32:41 +0200

    updated the doxygen

--- docs/search/variables_2.js
+++ docs/search/variables_2.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['equalize_5finbalance_0',['equalize_inbalance',['../namespacequids_1_1mpi.html#ab5de5240ccfc019063996bdc48e7a20b',1,'quids::mpi']]]
+  ['equalize_5fchildren_0',['equalize_children',['../namespacequids_1_1mpi.html#a20bc2eedd44dbcc296f16bcdd33ccec9',1,'quids::mpi']]],
+  ['equalize_5finbalance_1',['equalize_inbalance',['../namespacequids_1_1mpi.html#ab5de5240ccfc019063996bdc48e7a20b',1,'quids::mpi']]]
 ];
