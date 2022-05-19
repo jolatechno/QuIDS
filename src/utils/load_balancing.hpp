7cf3e68d6e326032669e729ad74ffd39ef294b6f
jolatechno <joseph.touzet@gmail.com>
2022-05-19 11:26:10 +0200

    corrected CCP

--- src/utils/load_balancing.hpp
+++ src/utils/load_balancing.hpp
@@ -7,44 +7,44 @@ namespace quids::utils {
 
 	/// simple CCP load balacing implementation.
 	template <class UnsignedIntIterator1, class UnsignedIntIterator2>
-	void inline load_balancing_from_prefix_sum(UnsignedIntIterator1 prefixSumLoadBegin, UnsignedIntIterator1 prefixSumLoadEnd,
+	void inline load_balancing_from_prefix_sum(const UnsignedIntIterator1 prefixSumLoadBegin, const UnsignedIntIterator1 prefixSumLoadEnd,
 		UnsignedIntIterator2 workSharingIndexesBegin, UnsignedIntIterator2 workSharingIndexesEnd) {
 
 		const size_t num_segments = std::distance(workSharingIndexesBegin, workSharingIndexesEnd) - 1;
-		const size_t num_buckets = std::distance(prefixSumLoadBegin, prefixSumLoadEnd);
+		const size_t num_buckets = std::distance(prefixSumLoadBegin, prefixSumLoadEnd) - 1;
 		
-		workSharingIndexesBegin[0] = 0;
-		workSharingIndexesBegin[num_segments] = num_buckets - 1;
+		std::fill(workSharingIndexesBegin, workSharingIndexesEnd - 1, 0);
+		workSharingIndexesBegin[num_segments] = num_buckets;
 
-		size_t w_min = prefixSumLoadBegin[num_buckets - 1]/num_segments;
+		size_t w_min = prefixSumLoadBegin[num_buckets]/num_segments;
 		size_t w_max = 2*w_min + 1;
 
 		std::vector<size_t> separators(num_segments - 1, 0);
 
 		/* probe function */
 		auto probe_load_sharing = [&](size_t wprobe) {
+			if (wprobe == 0 || wprobe >= prefixSumLoadBegin[num_buckets])
+				return true;
+
 			/* separators */
 			size_t last_separator = 0;
 			size_t last_separator_value = 0;
 
 			/* find separators */
 			for (size_t separator = 1; separator < num_segments; ++separator) {
-				if (prefixSumLoadBegin[num_buckets - 1] - last_separator_value > wprobe*(num_segments + 1 - separator))
-					return false;
-
 				/* dichotomie search of separator */
 				separators[separator - 1] = std::distance(prefixSumLoadBegin,
-					std::upper_bound(prefixSumLoadBegin + last_separator, prefixSumLoadBegin + num_buckets,
+					std::lower_bound(prefixSumLoadBegin + last_separator, prefixSumLoadEnd,
 						last_separator_value + wprobe)) - 1;
 
 				/* prepare next iteration */
 				last_separator = separators[separator - 1];
 				last_separator_value = prefixSumLoadBegin[last_separator];
-			}
 
-			/* check last segment */
-			if (prefixSumLoadBegin[num_buckets - 1] - last_separator_value > wprobe)
-				return false;
+
+				if (prefixSumLoadBegin[num_buckets] - last_separator_value > wprobe*(num_segments - separator))
+					return false;
+			}
 
 			/* copy separators over */
 			for (size_t i = 0; i < num_segments - 1; ++i)
